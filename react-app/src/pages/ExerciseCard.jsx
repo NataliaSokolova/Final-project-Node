aff0d8bbb4409a0c77d5bf5b06e959e452427b66
Natalia Sokolova <nataliya.a.sokolova@gmail.com>
2025-02-17 10:17:55 -0800

    commit

--- react-app/src/pages/ExerciseCard.jsx
+++ react-app/src/pages/ExerciseCard.jsx
@@ -15,7 +15,16 @@ const ExerciseCard = () => {
             <ul>
                 {exercises.map(exercise => (
                     <li key={exercise.id}>
-                       {exercise.id} {exercise.name} {exercise.isFavorite ? 
+                       {/* {exercise.id} */}
+
+
+                     <h3>  {exercise.name} </h3>  <br />
+                        {exercise.bodyPart} <br />
+                        {exercise.equipment} <br />
+                        {exercise.target} <br />
+                        {exercise.secondaryMuscles} <br />
+                        {exercise.instructions} <br />
+                        {exercise.isFavorite ? 
                        (<button>Remove from favorites</button>) :
                        (<button onClick={() => addToFav(exercise.id)}>Add to favorites</button>)}
                     </li>
